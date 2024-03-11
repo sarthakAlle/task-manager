7bf3ba577d6dbdff81669df30b15149535063ff5
Sarthak <sarthakt556@gmail.com>
2024-03-11 12:07:26 +0530

    added optionSuccessStatus:200 in index.js

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,9 @@ const app = express();
 
 connectDB();
 const corsConfig={
-  origin:"http://localhost:3000",
-  credential:true
+  origin:"*",
+  credential:true,
+  optionSuccessStatus:200,
 };
 // Use cors middleware to handle CORS headers
 app.use(cors(corsConfig));
@@ -25,12 +26,7 @@ app.use(cors(corsConfig));
 app.use(express.urlencoded({extended:true}));
 
 app.use(express.json())
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 //task controllers
 /*
 app.post('/tasks',isAuth, async (req, res) => {
