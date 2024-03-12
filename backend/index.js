7b4691997fe41431ecb84e68dd297b8b38481a6e
Sarthak <sarthakt556@gmail.com>
2024-03-12 09:56:08 +0530

    removed unnecsesary code

--- backend/index.js
+++ backend/index.js
@@ -15,40 +15,7 @@ connectDB();
 
 const app = express();
 const cors = require("cors");
-const corsOptions = {
-  origin: /\.onrender\.com$/,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
-app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://localhost:3000"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
-/*
-const corsConfig={
-  origin:"*",
-  credential:true,
-  optionSuccessStatus:200,
-};*/
-// Use cors middleware to handle CORS headers
-
-//app.options("",cors(corsConfig));
-
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
 
 app.use(express.json())
